437760dfadb05ecfc2272bacd88bfc206f3a43f8
dbhynds <dbhynds@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-26 21:39:54 +0000

    Added way more/better settings. Plus the ability to do html5
    git-svn-id: https://plugins.svn.wordpress.org/lazy-load-xt/trunk@1100721 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lazy-load-xt.php
+++ lazy-load-xt.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Lazy_Load_XT
- * @version 0.1
+ * @version 0.2
  */
 /*
 Plugin Name: Lazy Load XT for WordPress
@@ -9,7 +9,7 @@ Plugin URI: http://wordpress.org/plugins/lazy-load-xt/
 Description: Lazy load post images using Lazy Load XT
 Author: Davo Hynds
 Author URI: http://mightybytes.com
-Version: 0.1
+Version: 0.2
 Text Domain: lazy-load-xt
 */
 
@@ -18,40 +18,55 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 class LazyLoadXT {
 	
-	protected $dir;
-	protected $lazyloadxt_ver = '1.0.6';
-	protected $settingsClass;
-	protected $settings;
+	protected $dir; // Plugin directory
+	protected $lazyloadxt_ver = '1.0.6'; // Version of Lazy Load XT (the script, not this plugin)
+	protected $settingsClass; // Settings class for admin area
+	protected $settings; // Settings for this plugin
 
 	function __construct() {
-		
-		add_filter( 'the_content', array($this,'the_content_filter') );
-		add_filter( 'wp_get_attachment_image_attributes', array($this,'wp_get_attachment_image_attributes_filter') );
-		
-		//add_filter( 'get_image_tag', array($this,'get_image_tag_filter'), 10, 2);
-		add_action( 'wp_enqueue_scripts', array($this,'load_scripts') );
-		
-		if (is_admin()) {
-			require 'settings.php';
-			$settingsClass = new LazyLoadXTSettings;
-			register_activation_hook(__FILE__,array($settingsClass,'first_time_activation'));
-		}
 
+		// Store our settings in memory to reduce mysql calls
 		$this->settings = $this->get_settings();
 		$this->dir = plugin_dir_url(__FILE__);
 
-	}
+		// If we're in the admin area, load the settings class
+		if (is_admin()) {
+			require 'settings.php';
+			$settingsClass = new LazyLoadXTSettings;
+			// If this is the first time we've enabled the plugin, setup default settings
+			register_activation_hook(__FILE__,array($settingsClass,'first_time_activation'));
+		}
+		
+		// Enqueue Lazy Load XT scripts and styles
+		add_action( 'wp_enqueue_scripts', array($this,'load_scripts') );
+		
+		// Replace the 'src' attr with 'data-src' in the_content
+		add_filter( 'the_content', array($this,'the_content_filter') );
+		// If enabled replace the 'src' attr with 'data-src' in text widgets
+		if ($this->settings['textwidgets']) {
+			add_filter( 'widget_text', array($this,'the_content_filter') );
+		}
+		// If enabled replace the 'src' attr with 'data-src' in the_post_thumbnail
+		if ($this->settings['thumbnails']) {
+			add_filter( 'wp_get_attachment_image_attributes', array($this,'wp_get_attachment_image_attributes_filter') );
+		}
 
+	}
 
 	function get_settings() {
 
+		// Get setting options from the db
 		$general = get_option('lazyloadxt_general');
 		$effects = get_option('lazyloadxt_effects');
 		$addons = get_option('lazyloadxt_addons');
 
+		// Set the array of options
 		$settings_arr = array(
 				'minimize_scripts',
 				'load_extras',
+				'thumbnails',
+				'textwidgets',
+				'excludeclasses',
 				'fade_in',
 				'spinner',
 				'script_based_tagging',
@@ -61,7 +76,9 @@ class LazyLoadXT {
 				'deferred_load',
 			);
 
+		// Start fresh
 		$settings = array();
+		// Loop through the settings we're looking for, and set them if they exist
 		foreach ($settings_arr as $setting) {
 			if ($general && array_key_exists('lazyloadxt_'.$setting,$general)){
 				$return = $general['lazyloadxt_'.$setting];
@@ -70,102 +87,147 @@ class LazyLoadXT {
 			} elseif ($addons && array_key_exists('lazyloadxt_'.$setting,$addons)){
 				$return = $addons['lazyloadxt_'.$setting];
 			} else {
+				// Otherwise set the option to false
 				$return = false;
 			}
 			$settings[$setting] = $return;
 		}
+
+		$settings['excludeclasses'] = ($settings['excludeclasses']) ? explode(' ',$settings['excludeclasses']) : array();
+		
+		// Return the settings
 		return $settings;
 
 	}
 
-
 	
 	function load_scripts() {
+
+		// Are these minified?
 		$min = ($this->settings['minimize_scripts']) ? '.min' : '';
+		// Just to save space
 		$jqll = 'jquery.lazyloadxt';
 		
+		// Enqueue fade-in if enabled
 		if ( $this->settings['fade_in'] ) {
 			wp_enqueue_style( 'lazyloadxt-fadein-style', $this->dir.'css/'.$jqll.'.fadein'.$min.'.css', false, $this->lazyloadxt_ver );
 		}
+		// Enqueue spinner if enabled
 		if ( $this->settings['spinner'] ) {
 			wp_enqueue_style( 'lazyloadxt-spinner-style', $this->dir.'css/'.$jqll.'.spinner'.$min.'.css', false, $this->lazyloadxt_ver );
 		}
 		
+		// Enqueue extras enabled. Otherwise, load the regular script
 		if ( $this->settings['load_extras'] ) {
 			wp_enqueue_script( 'lazy-load-xt-script', $this->dir.'js/'.$jqll.'.extra'.$min.'.js', array( 'jquery' ), $this->lazyloadxt_ver );
 		} else {
 			wp_enqueue_script( 'lazy-load-xt-script', $this->dir.'js/'.$jqll.$min.'.js', array( 'jquery' ), $this->lazyloadxt_ver );
 		}
 
-		/*if ( $this->settings['script_based_tagging'] ) {
-		}
-		if ( $this->settings['responsive_images'] ) {
-		}*/
+		// Enqueue print if enabled
 		if ( $this->settings['print'] ) {
-			wp_enqueue_script( 'lazy-load-xt-script', $this->dir.'js/'.$jqll.'.print'.$min.'.js', array( 'jquery','lazy-load-xt-script' ), $this->lazyloadxt_ver );
+			wp_enqueue_script( 'lazy-load-xt-print', $this->dir.'js/'.$jqll.'.print'.$min.'.js', array( 'jquery','lazy-load-xt-script' ), $this->lazyloadxt_ver );
 		}
-		/*if ( $this->settings['background_image'] ) {
-		}*/
+		if ( $this->settings['background_image'] ) {
+			wp_enqueue_script( 'lazy-load-xt-bg', $this->dir.'js/'.$jqll.'.bg'.$min.'.js', array( 'jquery','lazy-load-xt-script' ), $this->lazyloadxt_ver );
+		}
+		// Enqueue deferred load if enabled
 		if ( $this->settings['deferred_load'] ) {
-			wp_enqueue_script( 'lazy-load-xt-script', $this->dir.'js/'.$jqll.'.autoload'.$min.'.js', array( 'jquery','lazy-load-xt-script' ), $this->lazyloadxt_ver );
+			wp_enqueue_script( 'lazy-load-xt-deferred', $this->dir.'js/'.$jqll.'.autoload'.$min.'.js', array( 'jquery','lazy-load-xt-script' ), $this->lazyloadxt_ver );
 		}
 		
 	}
 
 	function the_content_filter($content) {
-		$newcontent = $content;
-		$newcontent = $this->switch_src_for_data_src($newcontent,'img');
-		if ($this->settings['load_extras']) {
-			$newcontent = $this->switch_src_for_data_src($newcontent,'iframe');
+		// If there's anything there, replace the 'src' with 'data-src'
+		if (strlen($content)) {
+			$newcontent = $content;
+			// Replace 'src' with 'data-src' on images
+			$newcontent = $this->switch_src_for_data_src($newcontent,array('img'));
+			// If enabled, replace 'src' with 'data-src' on iframes
+			if ($this->settings['load_extras']) {
+				$newcontent = $this->switch_src_for_data_src($newcontent,array('iframe','embed','video','audio','source'));
+			}
+			return $newcontent;
+		} else {
+			// Otherwise, carry on
+			return $content;
 		}
-		return $newcontent;
 	}
 
-	function switch_src_for_data_src($content,$tag) {
+	function switch_src_for_data_src($content, $tags) {
+		// Make a new DOMDoc
 		$doc = new DOMDocument();
-		$doc->LoadHTML($content);
-		$elements = $doc->getElementsByTagName($tag);
+		// Load it up (Doesn't like HTML5)
+		@$doc->LoadHTML($content);
 
-		foreach ($elements as $element) {
-			$src = $element->getAttribute('src');
-			// Remove the existing attributes.
-			$element->removeAttribute('src');
-			// Set the new attribute.
-			$element->setAttribute('data-src', $src);
+		// Attributes to search for
+		$attrs = array('src','poster');
+		// Elements requiring a 'src' attribute to be valide HTML
+		$src_req = array('img','source');
+
+		foreach ($tags as $tag) {
+			// Get the elements we need to switch the src for
+			$elements = $doc->getElementsByTagName($tag);
+
+			// Switch out the 'src' with 'data-src'
+			foreach ($elements as $element) {
+				// Get the classes of element
+				if ($tag == 'source') {
+					// Check the parent tag for <video> and <audio> tags
+					$parent = $element->parentNode;
+					$classes = explode(' ',$parent->getAttribute('class'));
+				} else {
+					$classes = explode(' ',$element->getAttribute('class'));
+				}
+				
+				// If it doesn't have any of the designated "skip" classes, replace the 'src' with 'data-src'
+				if (count(array_intersect($classes,$this->settings['excludeclasses'])) == 0) {
+					foreach ($attrs as $attr) {
+						// Try to get the src attr
+						$elemattr = $element->getAttribute($attr);
+						//if attr exists
+						if ($elemattr) {
+							// If a 'src' attribute is required for valid html
+							if (in_array($tag,$src_req)) {
+								// Set the 'src' to a 1x1 pixel transparent gif
+								$element->setAttribute($attr,'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+							} else {
+								// Remove the existing attributes.
+								$element->removeAttribute($attr);
+							}
+    						// Set the new attribute.
+							$element->setAttribute('data-'.$attr, $elemattr);
+						}
+					}
+				}
+			}
 		}
 
+		// Prep for return
 		$return = new DOMDocument();
+		// Get the contents of the body tag
 		$body = $doc->getElementsByTagName('body')->item(0);
+		// Append them to the $return
 		foreach ($body->childNodes as $child){
 		    $return->appendChild($return->importNode($child, true));
 		}
+		// And we're done
 		return $return->saveHTML();
 	}
 
-	/*function get_image_tag_filter($html, $id, $alt, $align, $size) {
-	    list( $img_src, $width, $height ) = image_downsize($id, $size);
-	    $imagesize = getimagesize($img_src);
-
-	    $class = 'align' . esc_attr($align) .' size-' . esc_attr($size) . ' wp-image-' . $id;
-	    $html = '<img data-src="' . esc_attr($img_src) . '" alt="' . esc_attr($alt) . '" data-width="' . $imagesize[0] . '" data-height="' . $imagesize[1] . '" class="' . $class . '" />';
-	    $before = '';
-	    $after = '<noscript><img src="' . esc_attr($img_src) . '" alt="' . esc_attr($alt) . '" class="' . $class . '" /></noscript>';
-	    $html = $before . $html . $after;
-
-	    return $html;
-	}*/
-
-
 	function wp_get_attachment_image_attributes_filter($attr) {
+		// Change the attribute 'src' to 'data-src'
 		$attr['data-src'] = $attr['src'];
-		unset($attr['src']);
+		// Set 'src' to a 1x1 pixel transparent gif
+		$attr['src'] = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 		return $attr;
 	}
 
 }
 
-new LazyLoadXT;
-
+// Init
+$lazyloadxt = new LazyLoadXT;
 
 
 
--- settings.php
+++ settings.php
@@ -4,19 +4,56 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 class LazyLoadXTSettings {
 
+	protected $ver = '0.2.0'; // Plugin version
+	protected $defaults = array(
+			'general' => array(
+					'lazyloadxt_minimize_scripts' => 1,
+					'lazyloadxt_thumbnails' => 1,
+					'lazyloadxt_textwidgets' => 1,
+					'lazyloadxt_load_extras' => 0,
+					'lazyloadxt_excludeclasses' => '',
+					'lazyloadxt_spinner' => 0,
+					'lazyloadxt_deferred_load' => 0,
+					'lazyloadxt_background_image' => 0,
+				),
+		);
+
 	public function __construct() {
 		add_action( 'admin_menu', array($this,'lazyloadxt_add_admin_menu') );
 		add_action( 'admin_init', array($this,'lazyloadxt_settings_init') );
+		add_action( 'admin_enqueue_scripts', array($this,'lazyloadxt_enqueue_admin') );
+		add_action( 'upgrader_process_complete', array($this,'update') );
 	}
 
 	function first_time_activation() {
-		if ( !get_option('lazyloadxt_general') ) {
-			update_option('lazyloadxt_general',array('lazyloadxt_minimize_scripts'=>1));
+		// Set default settings
+		$defaults = $this->defaults;
+		foreach ($defaults as $key => $val) {
+			if (get_option('lazyloadxt_'.$key,false) != false) {
+				update_option('lazyloadxt_'.$key,$val);
+			}
+		}
+		update_option('lazyloadxt_version',$this->ver);
+	}
+	
+	function update() {
+		$defaults = $this->defaults;
+		$ver = $this->ver;
+		$dbver = get_option('lazyloadxt_version','');
+		if (version_compare($ver,$dbver,'>')) {
+			if (version_compare($dbver,'0.2','<=')) {
+				$this->first_time_activation();
+			}
+			update_option('lazyloadxt_version',$this->ver);
 		}
 	}
 
+
+
 	function lazyloadxt_add_admin_menu() { 
-		add_options_page( 'Lazy Load XT', 'Lazy Load XT', 'manage_options', 'lazyloadxt', array($this,'settings_page') );
+		$admin_page = add_options_page( 'Lazy Load XT', 'Lazy Load XT', 'manage_options', 'lazyloadxt', array($this,'settings_page') );
+	}
+	function lazyloadxt_enqueue_admin() {
 	}
 
 
@@ -70,13 +107,29 @@ class LazyLoadXTSettings {
 				<span><?php _e('Basic settings','lazy-load-xt'); ?></span>
 			</legend>
 			<label for="lazyloadxt_minimize_scripts">
-				<input type='checkbox' id='lazyloadxt_minimize_scripts' name='lazyloadxt_general[lazyloadxt_minimize_scripts]' <?php checked( $options['lazyloadxt_minimize_scripts'], 1 ); ?> value='1'>
+				<input type='checkbox' id='lazyloadxt_minimize_scripts' name='lazyloadxt_general[lazyloadxt_minimize_scripts]' <?php checked( $options['lazyloadxt_minimize_scripts'], 1 ); ?> value="1">
 				<?php _e('Load minimized versions of javascript and css files.','lazy-load-xt'); ?>
 			</label>
 			<br />
 			<label for="lazyloadxt_load_extras">
-				<input type='checkbox' id='lazyloadxt_load_extras' name='lazyloadxt_general[lazyloadxt_load_extras]' <?php checked( $options['lazyloadxt_load_extras'], 1 ); ?> value='1'>
+				<input type='checkbox' id='lazyloadxt_load_extras' name='lazyloadxt_general[lazyloadxt_load_extras]' <?php checked( $options['lazyloadxt_load_extras'], 1 ); ?> value="1">
 				<?php _e('Lazy load YouTube and Vimeo videos, iframes, audio, etc.','lazy-load-xt'); ?>
+			</label>
+			<br />
+			<label for="lazyloadxt_thumbnails">
+				<input type='checkbox' id='lazyloadxt_thumbnails' name='lazyloadxt_general[lazyloadxt_thumbnails]' <?php checked( $options['lazyloadxt_thumbnails'], 1 ); ?> value="1">
+				<?php _e('Lazy load post thumbnails.','lazy-load-xt'); ?>
+			</label>
+			<br />
+			<label for="lazyloadxt_textwidgets">
+				<input type='checkbox' id='lazyloadxt_textwidgets' name='lazyloadxt_general[lazyloadxt_textwidgets]' <?php checked( $options['lazyloadxt_textwidgets'], 1 ); ?> value="1">
+				<?php _e('Lazy load text widgets.','lazy-load-xt'); ?>
+			</label>
+			<br />
+			<label for="lazyloadxt_excludeclasses">
+				<?php _e('Skip lazy loading on these classes:','lazy-load-xt'); ?><br />
+				<textarea id='lazyloadxt_excludeclasses' name='lazyloadxt_general[lazyloadxt_excludeclasses]' rows="3" cols="60"><?php echo $options['lazyloadxt_excludeclasses']; ?></textarea>
+				<p class="description"><?php _e('Prevent objects with the above classes from being lazy loaded. (List classes separated by a space and without the proceding period. e.g. "skip-lazy-load size-thumbnail".)','lazy-load-xt'); ?></p>
 			</label>
 		</fieldset>
 		<?php
@@ -92,12 +145,12 @@ class LazyLoadXTSettings {
 				<span><?php _e('Effects settings','lazy-load-xt'); ?></span>
 			</legend>
 			<label for="lazyloadxt_fade_in">
-				<input type='checkbox' id='lazyloadxt_fade_in' name='lazyloadxt_effects[lazyloadxt_fade_in]' <?php checked( $options['lazyloadxt_fade_in'], 1 ); ?> value='1'>
+				<input type='checkbox' id='lazyloadxt_fade_in' name='lazyloadxt_effects[lazyloadxt_fade_in]' <?php checked( $options['lazyloadxt_fade_in'], 1 ); ?> value="1">
 				<?php _e('Fade in lazy loaded objects','lazy-load-xt'); ?>
 			</label>
 			<br />
 			<label for="lazyloadxt_spinner">
-				<input type='checkbox' id='lazyloadxt_spinner' name='lazyloadxt_effects[lazyloadxt_spinner]' <?php checked( $options['lazyloadxt_spinner'], 1 ); ?> value='1'>
+				<input type='checkbox' id='lazyloadxt_spinner' name='lazyloadxt_effects[lazyloadxt_spinner]' <?php checked( $options['lazyloadxt_spinner'], 1 ); ?> value="1">
 				<?php _e('Show spinner while objects are loading','lazy-load-xt'); ?>
 			</label>
 		</fieldset>
@@ -112,31 +165,21 @@ class LazyLoadXTSettings {
 			<legend class="screen-reader-text">
 				<span><?php _e('Addons settings','lazy-load-xt'); ?></span>
 			</legend>
-			<?php /* ?>
-			<label for="lazyloadxt_script_based_tagging">
-				<input type='checkbox' id='lazyloadxt_script_based_tagging' name='lazyloadxt_addons[lazyloadxt_script_based_tagging]' <?php checked( $options['lazyloadxt_script_based_tagging'], 1 ); ?> value='1'>
-
-			</label>
-			<br />
-			<label for="lazyloadxt_responsive_images">
-				<input type='checkbox' id='lazyloadxt_responsive_images' name='lazyloadxt_addons[lazyloadxt_responsive_images]' <?php checked( $options['lazyloadxt_responsive_images'], 1 ); ?> value='1'>
-			</label>
-			<br />
-			<?php */ ?>
 			<label for="lazyloadxt_print">
-				<input type='checkbox' id='lazyloadxt_print' name='lazyloadxt_addons[lazyloadxt_print]' <?php checked( $options['lazyloadxt_print'], 1 ); ?> value='1'>
-				<?php _e('Make sure lazy loaded elements appear in the print view','lazy-load-xt'); ?>
+				<input type='checkbox' id='lazyloadxt_print' name='lazyloadxt_addons[lazyloadxt_print]' <?php checked( $options['lazyloadxt_print'], 1 ); ?> value="1">
+				<?php _e('Make sure lazy loaded elements appear in the print view.','lazy-load-xt'); ?>
 			</label>
 			<br />
-			<?php /* ?>
-			<label for="lazyloadxt_background_image">
-				<input type='checkbox' id='lazyloadxt_background_image' name='lazyloadxt_addons[lazyloadxt_background_image]' <?php checked( $options['lazyloadxt_background_image'], 1 ); ?> value='1'>
-			</label>
-			<br />
-			<?php */ ?>
 			<label for="lazyloadxt_deferred_load">
-				<input type='checkbox' id='lazyloadxt_deferred_load' name='lazyloadxt_addons[lazyloadxt_deferred_load]' <?php checked( $options['lazyloadxt_deferred_load'], 1 ); ?> value='1'>
-				<?php _e('Defer loading of objects by 50ms','lazy-load-xt'); ?>
+				<input type='checkbox' id='lazyloadxt_deferred_load' name='lazyloadxt_addons[lazyloadxt_deferred_load]' <?php checked( $options['lazyloadxt_deferred_load'], 1 ); ?> value="1">
+				<?php _e('Defer loading of objects by 50ms.','lazy-load-xt'); ?>
+			</label>
+			<br />
+			<label for="lazyloadxt_background_image">
+				<input type='checkbox' id='lazyloadxt_background_image' name='lazyloadxt_addons[lazyloadxt_background_image]' <?php checked( $options['lazyloadxt_background_image'], 1 ); ?> value="1">
+				<?php _e('Lazy load background images.','lazy-load-xt'); ?>
+				<p class="description"><?php _e('Note: You must add the attribute "data-bg" with a value of path to the image to elements with a background image.','lazy-load-xt'); ?></p>
+				<p class="description"><?php _e('E.g. "&lt;div data-bg="/path/to/image.png"&gt;...&lt;/div&gt;"','lazy-load-xt'); ?></p>
 			</label>
 		</fieldset>
 		<?php
@@ -150,15 +193,13 @@ class LazyLoadXTSettings {
 
 	}
 
-	
-
 
 	function settings_page() { 
 
 		?>
 		<div class="wrap">
 			<h2><?php _e('Lazy Load XT'); ?></h2>
-			<form action='options.php' method='post'>
+			<form id="basic" action='options.php' method='post' style='clear:both;'>
 				<?php
 				settings_fields( 'basicSettings' );
 				do_settings_sections( 'basicSettings' );
